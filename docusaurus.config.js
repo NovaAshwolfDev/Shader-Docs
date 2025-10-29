b5d2426de45096d02e95909679d6a8de07862217
Nova Zephyr Ashwolf <novaashwolf@gmail.com>
2025-10-29 18:43:04 +1100

    CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
             items: [
               {
                 label: "Getting Started",
-                to: "/docs/intro",
+                to: "/docs/basics",
               },
             ],
           },
